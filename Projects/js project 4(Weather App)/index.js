018189ea3325609a14c6eb3ef3c0a7884e0f95ea
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2023-04-14 21:47:34 +0530

    14-04-2023

---
+++ Projects/js project 4(Weather App)/index.js
@@ -0,0 +1,8 @@
+const userTab = document.querySelector("[data-userWeather]");
+const searchtab = document.querySelector("[data-searchWeather]"); 
+const userContainer = document.querySelector(".weather-container");
+const grandAccessContainer = document.querySelector("grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
