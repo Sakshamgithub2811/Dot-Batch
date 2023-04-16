db3948c09af3fc5575cc763d18b712b322257c91
Saksham Shrivastava <111189871+Sakshamgithub2811@users.noreply.github.com>
2023-04-16 14:04:09 +0530

    15-04-2023

--- Projects/js project 4(Weather App)/index.js
+++ Projects/js project 4(Weather App)/index.js
@@ -1,8 +1,26 @@
 const userTab = document.querySelector("[data-userWeather]");
-const searchtab = document.querySelector("[data-searchWeather]"); 
+const searchTab = document.querySelector("[data-searchWeather]"); 
 const userContainer = document.querySelector(".weather-container");
 const grandAccessContainer = document.querySelector("grant-location-container");
 const searchForm = document.querySelector("[data-searchForm]");
 const loadingScreen = document.querySelector(".loading-container");
 const userInfoContainer = document.querySelector(".user-info-container");
 
+let currentTab = userTab;
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+currentTab.classList.add("current-tab");
+ 
+
+function switchTab(){
+    if()
+}
+
+
+userTab.addEventListener("click", ()=>{
+    //switching to another tab
+    switchTab();
+});
+
+searchTab.addEventListener("click",()=>{
+   switchTab();
+});
